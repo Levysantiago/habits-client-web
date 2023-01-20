d4c217019b5e04d1d02d06bccb098e23e706754d
Levy Santiago <levyssantiago@gmail.com>
2023-01-19 21:07:14 -0300

    feat: habit days colors range

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,17 @@
+interface IProgressBarProps {
+  progress: number;
+}
+
+export function ProgressBar(props: IProgressBarProps) {
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        className="h-3 rounded-xl bg-violet-600"
+        role="progressbar"
+        aria-valuenow={props.progress}
+        aria-label="Progresso de hábitos completados nesse dia"
+        style={{ width: `${props.progress}%` }}
+      ></div>
+    </div>
+  );
+}
